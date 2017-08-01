bbae07aa1a51d21a904c572f82a2349c0adff56b
growlf <growlf@thenetyeti.com>
2017-08-01 09:42:24 -0700

    alpine compatibility fixes

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,12 @@ FROM openjdk:8u131-jre-alpine
 VOLUME ["/server", "/plugins"]
 WORKDIR /server
 
+ENV BUNGEE_HOME=/server \
+    BUNGEE_BASE_URL=https://ci.md-5.net/job/BungeeCord \
+    MEMORY=512m \
+    UID=1000 \
+    GID=1000
+
 COPY *.sh /usr/bin/
 
 RUN apk --no-cache add curl
@@ -10,11 +16,10 @@ RUN apk --no-cache add bash
 
 EXPOSE 25577
 
-ENV BUNGEE_HOME=/server \
-    BUNGEE_BASE_URL=https://ci.md-5.net/job/BungeeCord \
-    MEMORY=512m \
-    UID=1000 \
-    GID=1000
+RUN set -x \
+	&& addgroup -g 1000 -S bungeecord \
+	&& adduser -u 1000 -D -S -G bungeecord bungeecord
 
-#ENTRYPOINT ["/usr/bin/run-bungeecord.sh"]
-ENTRYPOINT ["/bin/bash"]
+USER bungeecord
+
+ENTRYPOINT ["/usr/bin/run-bungeecord.sh"]
--- run-bungeecord.sh
+++ run-bungeecord.sh
@@ -18,25 +18,4 @@ fi
 echo "Setting initial memory to ${INIT_MEMORY:-${MEMORY}} and max to ${MAX_MEMORY:-${MEMORY}}"
 JVM_OPTS="-Xms${INIT_MEMORY:-${MEMORY}} -Xmx${MAX_MEMORY:-${MEMORY}} ${JVM_OPTS}"
 
-if [[ -n "$UID" ]]; then
-    #    -h DIR          Home directory
-    #    -G GRP          Add user to existing group
-    #    -S              Create a system user
-    #    -D              Don't assign a password
-    #    -H              Don't create home directory
-    #    -u UID          User id
-    #    -k SKEL         Skeleton directory (/etc/skel)
-    userAddArgs="-D -S -H -u $UID"
-
-    if [[ -n "$GID" ]]; then
-        userAddArgs="$userAddArgs -g $GID"
-        addgroup -g ${GID} bungeecord
-    fi
-
-    adduser -h ${BUNGEE_HOME} ${userAddArgs} bungeecord
-
-    chown -R bungeecord: ${BUNGEE_HOME}
-    sudo -u bungeecord java -- ${JVM_OPTS} -jar $BUNGEE_JAR "$@"
-else
-    exec java $JVM_OPTS -jar $BUNGEE_JAR "$@"
-fi
+exec java $JVM_OPTS -jar $BUNGEE_JAR "$@"
